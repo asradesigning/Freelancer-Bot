db5f2b660889edf075adba34757050949c222caf
Muhammad Ashar <93272953+asradesigning@users.noreply.github.com>
2025-01-30 10:55:11 +0500

    update

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, redirect, request, jsonify, current_ap
 from __oauth__ import oauth_bp, get_user, get_projects_with_bids, get_customize_bid, set_pricing, get_bidding_status
 from admin import admin_bp
 from extensions import db, oauth  # Import from extensions
+import MySQLdb
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_required, current_user
 from models import Users, Customize, Skills, Pricing, Bidding
